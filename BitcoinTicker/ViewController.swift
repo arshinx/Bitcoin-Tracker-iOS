cfc2dc8e38f7812a7cdd29338cd1b7cf2bbaccfa
arshin <arshinx@users.noreply.github.com>
2018-12-18 00:16:39 -0800

    Networking & Getting Data

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -44,51 +44,29 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
       print(finalURL)
    }
    
-//    
-//    //MARK: - Networking
-//    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
-//    //MARK: - JSON Parsing
-//    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+   
+   //MARK: - Networking
+   /***************************************************************/
 
+   func getBitcoinData(url: String) {
 
+     Alamofire.request(url, method: .get)
+      .responseJSON { response in
+         if response.result.isSuccess {
+            // Debug
+            print("Sucess! Got the weather data")
+            // Store JSON and Update data
+            let bitcoinJSON : JSON = JSON(response.result.value!)
+            self.updateBitcoinData(json: bitcoinJSON)
 
+          } else { // Error Handling
+            print("Error: \(String(describing: response.result.error))")
+            self.bitcoinPriceLabel.text = "Connection Issues"
+          }
+      }
+   }
+   
+
+   
 }
 
