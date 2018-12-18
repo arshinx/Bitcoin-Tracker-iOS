900ad2da0a7d6013586caf7f852112af63b85c15
Arshin Jain <arshinx@icloud.com>
2018-12-17 22:30:05 -0800

    UIPicker delegate & datasource

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -5,7 +5,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
