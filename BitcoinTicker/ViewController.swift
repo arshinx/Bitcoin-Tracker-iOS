26b3edc57af6b26e0dff35889ba13ed9042f5f7c
arshin <arshinx@users.noreply.github.com>
2018-12-18 00:23:23 -0800

    currency symbols list

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+   let currencySymbolArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
    var finalURL = ""
 
    // IBOutlets
